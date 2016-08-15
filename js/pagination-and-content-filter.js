c2b1517f79fa8c036858f5b47a876d6eb0e6d5f0
SebastianPeterAndersson <sebastianandersson@Sebastians-MacBook-Pro.local>
2016-08-16 01:10:06 +0200

    Second Github Draft
    Added some comments and the pagination links are now toggle-able!

--- js/pagination-and-content-filter.js
+++ js/pagination-and-content-filter.js
@@ -1,14 +1,14 @@
-// Draft 3
-
-
+// *** DONE ***
 // Use the filters-example.html file to guide your decision making. Using progressive enhancement, your work should affect the index.html file.
 // Since only 10 students should be shown at a time, your programming needs to calculate the number of pages needed and add the appropriate number
 // of links to the bottom of the page.
 
+
 //************* GLOBAL VARIABLES *************//
 
+
 // The 'PP' in studentsPerPage stands for 'studentsPerPage'
-var studentsPP = 10; // Only 10 students should be shown per pagination page.
+var studentsPP = 10;    // Only 10 students should be shown per pagination page.
 
 var pagesNeeded;
 
@@ -16,19 +16,17 @@ var pagPages;
 
 var $students = 0;
 
+var container = $(".page"); // The container that surrounds everything.
+
+var pagination = $("<div class='pagination'></div>"); // The pagination container.
+
+var $studentItem = $(".student-item");
+
 
 //************* FUNCTIONS *************//
 
+
 // calculate the amount of jquery student items per page shown.
-
-function hideStudents() {
-    $(".student-list").each(function() {
-        $(this).hide();
-    });
-    console.log("Hiding ALL jquery students.");
-}
-// Calculates the jquery .student-items that are exclusively visible on the page.
-
 function calculate$students() {
     $(".student-item:visible").each(function(){
         $students++;
@@ -36,20 +34,58 @@ function calculate$students() {
     console.log("Calculated amount of student-items shown. There are " + $students + " of them.")
 }
 
+
+// Calculates the amount of pagination pages needed based on the number of students.
 function calculatePagesNeeded() {
     pagesNeeded = Math.ceil($students / studentsPP);
     console.log("You need " + pagesNeeded  + " pages for the pagination");
 }
 
-function addPages() {
 
+//Hides all jquery students on the page.
+function hideStudents() {
+    $studentItem.each(function() {
+        $(this).hide();
+    });
+    console.log("Hiding ALL jquery students.");
+}
+// Calculates the jquery .student-items that are exclusively visible on the page.
+
+
+// Appending the right number of Pagination Pages to the bottom of the page.
+function addPages() {
+    container.append(pagination); // Initially placing the pagination withing the container.
+
+    // For each page that is needed, append a pagination anchor to the pagination contiainer.
+    for ( i = 1; i < pagesNeeded + 1; i++ ) {   // I am adding one to both variables since I don't wish to have the pagination start at 0.
+        pagButton = $("<li><a href='#'>" + i + "</a></li>");
+        pagination.append(pagButton);
+    }
+
+    $(".pagination a:first").addClass("active");
+
+    console.log("Created " + i + " pagination pages and added .active class to the first anchor.");
+}
+
+
+// Function for the pagination anchor click.
+function paginationClicked() {
+    console.log("Pagination button Clicked")
+    $(this).parent().parent().children().children().removeClass("active");
+    $(this).addClass("active");
 }
 
 
 // Hide all but the first 10 students when the page loads once the students are counted.
-    calculate$students(); // Calling this function first since the students are shown on the page initially.
+// Calling this function first since the students are shown on the page initially.
+    calculate$students();
     hideStudents();
     calculatePagesNeeded();
+    addPages();
+
+
+    //Click function that reacts when an anchor is clicked.
+    $(".pagination a").click(paginationClicked);   // Calls function when a pagination anchor is clicked.
 
 
 
