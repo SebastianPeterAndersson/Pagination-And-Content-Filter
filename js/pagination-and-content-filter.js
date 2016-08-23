4e5a40f8e7d47a5ed3f797f43eb5f2a42e07125c
SebastianPeterAndersson <sebastianandersson@Sebastians-MacBook-Pro.local>
2016-08-24 00:30:17 +0200

    Search function update
    Search function kinda works now. Still some work to be done.

--- js/pagination-and-content-filter.js
+++ js/pagination-and-content-filter.js
@@ -16,17 +16,27 @@ var pagPages;
 
 var $students = 0;
 
-var container = $(".page"); // The container that surrounds everything.
+var pageContainer = $(".page"); // The container that surrounds everything.
 
 var pagination = $("<div class='pagination'></div>"); // The pagination container.
 
 var $studentItem = $(".student-item");
 
 //Creating an array of the student items that can be accessed any time.
-var studentArray = $.makeArray($studentItem);
+
 
 // The pagination anchor clicked
 var pagSelected;
+
+var container = $("<div></div>");
+
+var searchInput = $("<input></input>");
+
+var searchButton = $("<button></button>");
+
+var studentsFoundArr;
+
+//What was searched in the input search.
 
 
 //************* FUNCTIONS *************//
@@ -65,7 +75,7 @@ function showStudents() {
 
 // Appending the right number of Pagination Pages to the bottom of the page.
 function addPages() {
-    container.append(pagination); // Initially placing the pagination withing the container.
+    pageContainer.append(pagination); // Initially placing the pagination withing the container.
 
     // For each page that is needed, append a pagination anchor to the pagination contiainer.
     for (var i = 1; i < pagesNeeded + 1; i++ ) {   // I am adding one to both variables since I don't wish to have the pagination start at 0.
@@ -116,22 +126,116 @@ function paginate(x) {
 
 }
 
+function paginateSearch() {
+
+}
+
+//Constructs a search function.
+function constructSearch() {
+
+    searchInput.attr("placeholder", "Search students");
+    searchButton.text("Go!");
+    container.addClass("student-search");
+
+
+    $(".page-header").prepend(container);
+    container.append(searchInput).append(searchButton);
+    console.log(container);
+}
+
+
 // Function that automatically shows the first 10 students in the student list array.
 function paginateInitial() {
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 9; i++) {
         $(".student-item").eq(i).fadeIn("fast", function(){
-
+            // Nothing needed here
         });
     }
 }
 
+//May have to be changed (most certainly)
+function removePagPages () {
+    $(".pagination a").remove();
+}
+
+
+function searchButtonClicked() {
+    // this is what was put in the input field.
+    var userSearch = searchInput.val();
+    console.log('"' + userSearch + '"' + ' was searched');
+
+    //creating an array to store the students
+    studentsFoundArr = [];
+
+    // Variable to prevent the students to be hidden if nothing is searched
+    var preventHide = false;
+
+    //function to cycle thorugh the student items
+    $(".student-item").each(function(){
+        var studentName = $(this).find("h3").text();
+        var indexSearch = studentName.indexOf(userSearch);
+
+        // if student are not found
+        if (indexSearch === -1) {
+            console.log("Student NOT found");
+
+        }
+            // if search input is empty
+            else if (searchInput.val().length === 0) {
+                console.log("Nothing was searched");
+                preventHide = true;
+
+            }
+            // if at least one student is found
+            else if (indexSearch > -1) {
+                studentsFoundArr.push($(this));
+                console.log("Student found and pushing to array");
+            }
+
+    });
+
+    if ( preventHide === false) {
+        hideStudents();
+    } else {
+        console.log("Do nothing");
+    }
+
+    if (studentsFoundArr.length === 0) {
+        $(".student-list").append($("<p>Sorry. No student found. Try again!</p>"));
+    }
+
+    else if (studentsFoundArr.length > 0) {
+
+        var newHTML = [];
+
+        for ( var i = 0; i < studentsFoundArr.length; i++) {
+            $(".student-list").prepend("<li class='student-item cf'>" + studentsFoundArr[i].html() + "</li>");
+        }
+
+
+
+    }
 
 
 
 
-// Hide all but the first 10 students when the page loads once the students are counted.
-// Calling this function first since the students are shown on the page initially.
+    console.log(studentsFoundArr);
 
+}
+
+
+function restart() {
+    calculate$students();
+    hideStudents();
+    calculatePagesNeeded();
+    addPages();
+    console.log("Restarted");
+}
+
+
+
+    // Hide all but the first 10 students when the page loads once the students are counted.
+    // Calling this function first since the students are shown on the page initially.
     calculate$students();
 
     hideStudents();
@@ -140,22 +244,22 @@ function paginateInitial() {
 
     addPages();
 
+    // When a user clicks on “2” in the pagination, students 11 through 20 are shown. When a user clicks “3”, students 21 through 30 are shown. And so
+    // on. When “6” is clicked 51 through 55 should be shown.
     paginateInitial();
 
 
-
-
-
-
-
+    constructSearch();
+    // Using progressive enhancement, add the student search markup as presented in the filters-example.html
+    // file to the index.html file.
 
 
     //Click function that reacts when an anchor is clicked.
     $(".pagination a").click(paginationClicked);   // Calls function when a pagination anchor is clicked.
 
 
-    // When a user clicks on “2” in the pagination, students 11 through 20 are shown. When a user clicks “3”, students 21 through 30 are shown. And so
-    // on. When “6” is clicked 51 through 55 should be shown.
+    // Add an event listener to the search button.
+    searchButton.click(searchButtonClicked);
 
 
 
@@ -164,10 +268,11 @@ function paginateInitial() {
 
 
 
-// Using progressive enhancement, add the student search markup as presented in the filters-example.html file to the index.html file.
 
 
-// Add an event listener to the search button. When the user clicks on the button it should use the text in the search input to filter the results.
+
+
+
 // Searching should be case insensitive. e.g. a search for “Susan” should return results for “susan” and “Susan".
 
 
@@ -189,3 +294,53 @@ function paginateInitial() {
 // Mozilla Firefox
 // Internet Explorer/Edge
 // Safari
+
+
+//Did not feel like rmeoving the code all together in case It's needed for further Use
+
+// function searchButtonClicked() {
+//     hideStudents();
+//     // When the user clicks on the button it should use the text in the search input to filter the results.
+//     var searched = searchInput.val();
+//     console.log(searched);
+//
+//
+//     //counts how many students that are found
+//     var notFoundCount = 0;
+//     var foundCount = 0;
+//
+//     studentsFoundArr = [];
+//
+//     $(".student-item").each(function() {
+//
+//         if (searchInput.val().length === 0) {
+//             calculate$Students();
+//             paginateInitial();
+//         }
+//
+//         var studentName = $(this).find("h3").text();
+//         var indexSearch = studentName.indexOf(searched);
+//         //If indexOf reaches the conclusion that the string did not match, nothing is found.
+//         if ( indexSearch === -1 ) {
+//             notFoundCount++;
+//             $(this).hide();
+//         }
+//
+//
+//         else {
+//             $(this).show();
+//             foundCount++;
+//
+//         }
+//     });
+//
+//
+//
+//     // If nothing is found, start over.
+//     if (foundCount === $students) {
+//
+//     }
+//     console.log("Students not found: " + notFoundCount);
+//     console.log("Students found: " + foundCount);
+//
+// }
